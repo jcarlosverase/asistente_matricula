f2ced7c2e9e81bc80578d198db09aac144c78966
abelquispeorellana <abelquispeorellana1994@gmail.com>
2019-04-08 16:19:41 -0500

    Consulta de reporte

---
+++ AsistenteMatricula.Portal/Controllers/ReporteController.cs
@@ -0,0 +1,29 @@
+﻿using AsistenteMatricula.Portal.ReportesService;
+using AsistenteMatricula.Portal.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsistenteMatricula.Portal.Controllers
+{
+    public class ReporteController : Controller
+    {
+        [HttpGet]
+        [SecurityRequired] 
+        public ActionResult RankingDocentes()
+        {
+            return View(new List<Valoracion>());
+        }
+
+        [HttpGet]
+        [SecurityRequired]
+        public ActionResult RankingDocentesFilterValueDataList(DateTime FechaInicio, DateTime FechaFina)
+        {
+            var Result = new ReportesService.ReportesServiceClient().RankingDocentes(FechaInicio, FechaFina).ToList(); 
+            return PartialView("RankingDocentesPartial", Result);
+        }
+
+    }
+}
