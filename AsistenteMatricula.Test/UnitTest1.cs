9ae1f1887e43bb134cb6f43aa3ad8cb740bffde1
abelquispeorellana <abelquispeorellana1994@gmail.com>
2019-04-08 16:20:49 -0500

    prueba test asistente matricula

---
+++ AsistenteMatricula.Test/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel;
+using AsistenteMatricula.Test.Errores;
+
+namespace AsistenteMatricula.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            try
+            {
+
+                var instLocal = new GPUniversidadService.UniversidadUsuarioServiceClient().Crear(new
+                    GPUniversidadService.UniversidadUsuario()
+                {
+                    Correo = "orellana@upc.edu.pe",
+                    RUC = "12345678912",
+                    Nombre = "Alfredo",
+                    Apellido = "Orellana",
+                    Contrasenia ="123"
+                });
+                Assert.AreEqual("12345678912", instLocal.RUC);
+                Assert.AreEqual("Alfredo", instLocal.Nombre);
+
+            }
+            catch (FaultException<RepetidoException> error)
+            {
+                throw new Exception(error.Detail.Descripcion);
+            }
+        }
+    }
+}
