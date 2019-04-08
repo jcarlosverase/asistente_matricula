1fbb1d724742859d8c620986738954af4ecca8de
abelquispeorellana <abelquispeorellana1994@gmail.com>
2019-04-08 17:23:52 -0500

    Envento de enviar correo

---
+++ WCF_NotificacionService/EnviarCorreo.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_NotificacionService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "EnviarCorreo" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione EnviarCorreo.svc o EnviarCorreo.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class EnviarCorreo : IEnviarCorreo
+    { 
+        public void Enviar(string Correo, string Asunto, string Mensaje, byte[] Archivo = null, string NombreArchivo = "")
+        { 
+            var instMail = new Mail("smtp.gmail.com", "aqonotifications@gmail.com", "AqoN2017%");
+            instMail.ValorDelReceptor(Correo, Asunto, Mensaje);
+            if (Archivo != null && NombreArchivo.Length > 0)
+                instMail.AgregarArchivoByte(Archivo, NombreArchivo);
+            instMail.EnviarEmail();
+        }
+    }
+}
---
+++ WCF_NotificacionService/IEnviarCorreo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_NotificacionService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IEnviarCorreo" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IEnviarCorreo
+    {
+        [OperationContract]
+        void Enviar(string Correo, string Asunto, string Mensaje, byte[] Archivo, string NombreArchivo);
+    }
+}
