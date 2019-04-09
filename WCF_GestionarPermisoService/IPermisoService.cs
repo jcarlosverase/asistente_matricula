7ae04a2a6ed4d6adbaac77ee5f4595032494162e
abelquispeorellana <abelquispeorellana1994@gmail.com>
2019-04-08 22:33:40 -0500

    Actualizado el servicio de gestionar permiso

---
+++ WCF_GestionarPermisoService/IPermisoService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WCF_GestionarPermisoService.Dominio;
+using WCF_GestionarPermisoService.Errores;
+
+namespace WCF_GestionarPermisoService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IUniversidadService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IPermisoService
+    {
+        #region Universidad
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        Universidad UniversidadCrear(Universidad Parametro);
+
+        [OperationContract]
+        Universidad UniversidadObtener(string RUC);
+
+        [OperationContract]
+        Universidad UniversidadModificar(Universidad Parametro);
+
+        [OperationContract]
+        void UniversidadEliminar(string RUC);
+
+        [OperationContract]
+        List<Universidad> UniversidadListar();
+        #endregion
+
+        #region Universidad Usuario
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        UniversidadUsuario UniversidadUsuarioCrear(UniversidadUsuario Parametro);
+
+        [OperationContract]
+        UniversidadUsuario UniversidadUsuarioObtener(string Correo);
+
+        [OperationContract]
+        UniversidadUsuario UniversidadUsuarioModificar(UniversidadUsuario Parametro);
+
+        [OperationContract]
+        void UniversidadUsuarioEliminar(string Correo);
+
+        [OperationContract]
+        List<UniversidadUsuario> UniversidadUsuarioListar();
+        #endregion
+    }
+}
---
+++ WCF_GestionarPermisoService/PermisoService.svc.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WCF_GestionarPermisoService.Dominio;
+using WCF_GestionarPermisoService.Errores;
+using WCF_GestionarPermisoService.Persistencia;
+
+namespace WCF_GestionarPermisoService
+{
+    public class PermisoService : IPermisoService
+    {
+        #region Universidad
+
+        public Universidad UniversidadCrear(Universidad Parametro)
+        {
+            var DAO = new UniversidadDAO();
+            if (DAO.Obtener(Parametro.RUC) != null) // Ya existe
+            {
+                throw new FaultException<RepetidoException>
+                    (
+                        new RepetidoException()
+                        {
+                            Codigo = "101",
+                            Descripcion = "El RUC ya existe"
+                        },
+                        new FaultReason("Error al intentar creación")
+                    );
+            }
+            return DAO.Crear(Parametro);
+        }
+
+        public void UniversidadEliminar(string RUC)
+        {
+            var DAO = new UniversidadDAO();
+            DAO.Eliminar(RUC);
+        }
+
+        public List<Universidad> UniversidadListar()
+        {
+            var DAO = new UniversidadDAO();
+            return DAO.Listar();
+        }
+
+        public Universidad UniversidadModificar(Universidad Parametro)
+        {
+            var DAO = new UniversidadDAO();
+            return DAO.Modificar(Parametro);
+        }
+
+        public Universidad UniversidadObtener(string RUC)
+        {
+            var DAO = new UniversidadDAO();
+            return DAO.Obtener(RUC);
+        }
+        #endregion
+
+        #region Universidad Usuario
+         
+        public UniversidadUsuario UniversidadUsuarioCrear(UniversidadUsuario Parametro)
+        {
+            var DAO = new UniversidadUsuarioDAO();
+            if (DAO.Obtener(Parametro.Correo) != null) // Ya existe
+            {
+                throw new FaultException<RepetidoException>
+                    (
+                        new RepetidoException()
+                        {
+                            Codigo = "101",
+                            Descripcion = "El Correo ya existe"
+                        },
+                        new FaultReason("Error al intentar creación")
+                    );
+            }
+            return DAO.Crear(Parametro);
+        }
+
+        public void UniversidadUsuarioEliminar(string Correo)
+        {
+            var DAO = new UniversidadUsuarioDAO();
+            DAO.Eliminar(Correo);
+        }
+
+        public List<UniversidadUsuario> UniversidadUsuarioListar()
+        {
+            var DAO = new UniversidadUsuarioDAO();
+            return DAO.Listar();
+        }
+
+        public UniversidadUsuario UniversidadUsuarioModificar(UniversidadUsuario Parametro)
+        {
+            var DAO = new UniversidadUsuarioDAO();
+            return DAO.Modificar(Parametro);
+        }
+
+        public UniversidadUsuario UniversidadUsuarioObtener(string Correo)
+        {
+            var DAO = new UniversidadUsuarioDAO();
+            return DAO.Obtener(Correo);
+        }
+        #endregion
+    }
+}
