8e378343698c27546a81e0d3c74f6909463d5b5c
Juan Carlos <jcarlosverase@gmail.com>
2019-04-29 07:46:51 -0500

    Cambios Azure

--- WCF_GestionarAsistenteService/GestionarAsistenteService.svc.cs
+++ WCF_GestionarAsistenteService/GestionarAsistenteService.svc.cs
@@ -1,4 +1,5 @@
-﻿using WCF_GestionarAsistenteService;
+﻿using System;
+using WCF_GestionarAsistenteService;
 
 namespace WCF_GestionarAsistenteService
 {
@@ -6,16 +7,36 @@ namespace WCF_GestionarAsistenteService
     {
         public ValoracionService.RespuestaValoracion RegistrarValoracion(int idAlumno, int idDocente, int idCurso, int estrellas, string titulo, string resenia)
         {
-            ValoracionService.ValoracionServiceClient proxyValoracion = new ValoracionService.ValoracionServiceClient();
-            ValoracionService.RespuestaValoracion respuesta = proxyValoracion.Registrar(idAlumno, idDocente, idCurso, estrellas, titulo, resenia);
+            ValoracionService.RespuestaValoracion respuesta;
+            try
+            {
+                ValoracionService.ValoracionServiceClient proxyValoracion = new ValoracionService.ValoracionServiceClient();
+                respuesta = proxyValoracion.Registrar(idAlumno, idDocente, idCurso, estrellas, titulo, resenia);
+            }
+            catch (Exception ex)
+            {
+                respuesta = new ValoracionService.RespuestaValoracion();
+                respuesta.Codigo = -1;
+                respuesta.Mensaje = ex.Message;
+            }
 
             return respuesta;
         }
 
         public ValoracionService.RespuestaValoracion RankingValoracionDocentes(string nombreCurso)
         {
-            ValoracionService.ValoracionServiceClient proxyValoracion = new ValoracionService.ValoracionServiceClient();
-            ValoracionService.RespuestaValoracion respuesta = proxyValoracion.RankingDocentes(nombreCurso);
+            ValoracionService.RespuestaValoracion respuesta;
+            try
+            {
+                ValoracionService.ValoracionServiceClient proxyValoracion = new ValoracionService.ValoracionServiceClient();
+                respuesta = proxyValoracion.RankingDocentes(nombreCurso);
+            }
+            catch (Exception ex)
+            {
+                respuesta = new ValoracionService.RespuestaValoracion();
+                respuesta.Codigo = -1;
+                respuesta.Mensaje = ex.Message;
+            }
 
             return respuesta;
         }
